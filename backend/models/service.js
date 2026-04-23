d73c9fbe295eb66f695d07e1664df487b4e37ca3
IT21839160 <it21839160@my.sliit.lk>
2026-04-23 08:57:44 +0530

    service model

---
+++ backend/models/service.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const serviceSchema = new mongoose.Schema({
+  serviceName: {
+    type: String,
+    required:true
+  },
+    description: {
+    type: String,
+    required:true
+  },
+    price: {
+    type: Number,
+    required:true
+  },
+    duration:{
+    type: String,
+    },
+    createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Service = mongoose.model('Service', serviceSchema);
+
+module.exports = Service;
