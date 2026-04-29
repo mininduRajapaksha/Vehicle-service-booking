5ed48d934600f757ed4de3d98ca3b37a89e0a394
IT21839160 <it21839160@my.sliit.lk>
2026-04-29 19:13:01 +0530

    add contact us screen

---
+++ frontend/app/(publicTabs)/contact.tsx
@@ -0,0 +1,9 @@
+import { View,Text,StyleSheet } from "react-native";
+
+export default function contact(){
+    return(
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>Contact Us</Text>
+        </View>
+    );
+};
