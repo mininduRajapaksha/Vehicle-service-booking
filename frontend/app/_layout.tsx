b53e0a75adc2b44286de40183c6e86fa030076f3
IT21839160 <it21839160@my.sliit.lk>
2026-04-29 07:58:20 +0530

    role navigation

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,13 +1,36 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Stack } from "expo-router";
+import { useEffect, useState } from "react";
 
-export default function RootLayout() {
+
+export default function Layout() {
+  const [initialRoute, setInitialRoute] = useState<string | null>(null);
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
+  const checkUser = async () => {
+    const token = await AsyncStorage.getItem("token");
+    const role = await AsyncStorage.getItem("role");
+
+    if (!token) {
+      setInitialRoute("login");
+    } else if (role === "admin") {
+      setInitialRoute("admin");
+    } else {
+      setInitialRoute("(tabs)");
+    }
+  };
+
+  if (!initialRoute) return null;
+
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="tabs" options={{ headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="login" />
+      <Stack.Screen name="register" />
+      <Stack.Screen name="tabs" />
+      <Stack.Screen name="admin" />
     </Stack>
   );
 }
