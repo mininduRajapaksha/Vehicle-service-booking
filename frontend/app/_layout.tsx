4a412a1c864a2b5c326fa27ac9665c8966bfdf93
IT21839160 <it21839160@my.sliit.lk>
2026-04-29 17:46:32 +0530

    changes

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,36 +1,24 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Stack } from "expo-router";
-import { useEffect, useState } from "react";
-
 
 export default function Layout() {
-  const [initialRoute, setInitialRoute] = useState<string | null>(null);
-
-  useEffect(() => {
-    checkUser();
-  }, []);
-
-  const checkUser = async () => {
-    const token = await AsyncStorage.getItem("token");
-    const role = await AsyncStorage.getItem("role");
-
-    if (!token) {
-      setInitialRoute("login");
-    } else if (role === "admin") {
-      setInitialRoute("admin");
-    } else {
-      setInitialRoute("(tabs)");
-    }
-  };
-
-  if (!initialRoute) return null;
-
   return (
     <Stack screenOptions={{ headerShown: false }}>
+      
+      {/* Entry point */}
+      <Stack.Screen name="index" />
+
+      {/* Public (before login) */}
+      <Stack.Screen name="(publicTabs)" />
+
+      {/* Auth screens */}
       <Stack.Screen name="login" />
       <Stack.Screen name="register" />
+
+      {/* After login */}
       <Stack.Screen name="tabs" />
       <Stack.Screen name="admin" />
+
     </Stack>
+    
   );
 }
