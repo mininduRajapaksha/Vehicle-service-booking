9d9b1ca7ea3960477221c2b80383d513c0bed26f
IT21839160 <it21839160@my.sliit.lk>
2026-04-30 20:30:30 +0530

    route for delete

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 const router = express.Router();
 
-import { register, login, updateProfile } from "../controllers/authController.js";
+import { register, login, updateProfile, deleteAccount} from "../controllers/authController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 //public routes
 router.post("/register", register);
 router.post("/login", login);
 router.put("/update",protect, updateProfile)
+router.delete("/delete", protect, deleteAccount);
 
 //protected route example
 router.get("/profile", protect, (req, res) => {
