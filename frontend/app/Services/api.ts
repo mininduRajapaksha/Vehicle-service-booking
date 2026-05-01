419ce213596d5939dd81767868adaa7a2dc2729b
IT21839160 <it21839160@my.sliit.lk>
2026-05-01 21:32:41 +0530

    update hosted url

--- frontend/app/Services/api.ts
+++ frontend/app/Services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://192.168.1.3:5000/",
+  baseURL: "https://vehicle-service-booking-dpzu.onrender.com",
+  // "http://192.168.1.3:5000/",
 //   baseURL: "http://localhost:5000/",
 });
 
