1f442938d06d01be0b41f8bdafeedb54d702578d
mininduRajapaksha <rajapakshaminindu@gmail.com>
2026-06-20 12:14:23 +0530

    Switch to CloudinaryStorage for file uploads

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,15 +1,26 @@
 import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, file.fieldname + "-" + uniqueSuffix + "-" + file.originalname);
-    }
+const storage = new CloudinaryStorage({
+  cloudinary,
+
+  params: async (req, file) => ({
+    folder: "vehicle-services",
+
+    allowed_formats: [
+      "jpg",
+      "jpeg",
+      "png",
+      "webp"
+    ],
+
+    public_id: `${Date.now()}`
+  }),
 });
 
-const upload = multer({storage });
+const upload = multer({
+  storage,
+});
 
 export default upload;
